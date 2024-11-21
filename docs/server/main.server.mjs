d53aad4178ce18549673aa1ad96386ac8ff08acd
phuc.nguyendinh <ndp2193@gmail.com>
2024-11-21 16:15:00 +0700

    Re-build docs folder

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6QQHW3FU.mjs";import"./chunk-M7JKHXOH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-R26OR525.mjs";import"./chunk-M7JKHXOH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
