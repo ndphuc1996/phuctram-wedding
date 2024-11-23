428a8f95f9484a8686c4dfcaa99065b8b9f4230d
phuc.nguyendinh <ndp2193@gmail.com>
2024-11-24 00:58:17 +0700

    Add image

--- src/app/shared/components/wedding-dairy/wedding-dairy.component.ts
+++ src/app/shared/components/wedding-dairy/wedding-dairy.component.ts
@@ -9,5 +9,71 @@ import { CommonModule } from '@angular/common';
 })
 export class WeddingDairyComponent {
 
-
+ data = [
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-15.jpg',
+      imgTitle: '21.04.2023',
+      layout: 'col-md-4'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-26.jpg',
+      imgTitle: '07.05.2023',
+      layout: 'col-md-3'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-23.jpg',
+      imgTitle: '05.06.2023',
+      layout: 'col-md-4'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-10.jpg',
+      imgTitle: '12.10.2023',
+      layout: 'col-md-3'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-21.jpg',
+      imgTitle: '30.12.2023',
+      layout: 'col-md-3'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-20.jpg',
+      imgTitle: '13.01.2024',
+      layout: 'col-md-3'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-18.jpg',
+      imgTitle: '18.03.2024',
+      layout: 'col-md-4'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-14.jpg',
+      imgTitle: '20.03.2024',
+      layout: 'col-md-4'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-8.jpg',
+      imgTitle: '25.05.2024',
+      layout: 'col-md-4'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-9.jpg',
+      imgTitle: '25.05.2024',
+      layout: 'col-md-4'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-3.jpg',
+      imgTitle: '15.06.2024',
+      layout: 'col-md-4'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-1.jpg',
+      imgTitle: '15.08.2024',
+      layout: 'col-md-3'
+    },
+    {
+      imgUrl: 'assets/params/images/dairy/phuctram-dairy-27.jpg',
+      imgTitle: '15.08.2024',
+      layout: 'col-md-4'
+    }
+ ]
 }
--- src/app/shared/components/wedding-gallery/wedding-gallery.component.ts
+++ src/app/shared/components/wedding-gallery/wedding-gallery.component.ts
@@ -9,5 +9,54 @@ import { CommonModule } from '@angular/common';
 })
 export class WeddingGalleryComponent {
 
-
+  data = [
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-3.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-4.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-9.jpg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-6.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-red-1.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-red-2.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-red-3.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-red-1.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-normal-1.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-white-normal.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-normal-2.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-normal-3.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-yellow-1.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-yellow-2.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-yellow-3.jpeg'
+    },
+    {
+      imgUrl: 'assets/params/images/wedding/phuctram-bg-yellow-4.jpeg'
+    },
+  ]
 }
