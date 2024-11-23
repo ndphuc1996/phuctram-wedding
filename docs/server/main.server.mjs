64aba2abe32a1b4ffcf92ea2e0238b8e077b0533
phuc.nguyendinh <ndp2193@gmail.com>
2024-11-23 18:36:15 +0700

    Change image path

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YDEW4IU3.mjs";import"./chunk-TNQSJJ2X.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4TND3W7F.mjs";import"./chunk-TNQSJJ2X.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
