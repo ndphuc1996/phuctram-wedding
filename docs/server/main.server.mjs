974775676ca89757406364daba52febc690d5eed
phuc.nguyendinh <ndp2193@gmail.com>
2024-11-23 16:42:31 +0700

    Update style

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DREOUYKH.mjs";import"./chunk-M7JKHXOH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-UL5554T7.mjs";import"./chunk-TNQSJJ2X.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
