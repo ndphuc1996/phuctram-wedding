cebba07c5f738f498ac0ed8293806c6d596e6c47
phuc.nguyendinh <ndp2193@gmail.com>
2024-11-23 20:36:24 +0700

    Change path

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KU7GI4JG.mjs";import"./chunk-TNQSJJ2X.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HHSLAU2E.mjs";import"./chunk-TNQSJJ2X.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
