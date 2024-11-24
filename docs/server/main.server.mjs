4522ff592d95ff9dc0a6a7a930270c6de1a5e49a
phuc.nguyendinh <ndp2193@gmail.com>
2024-11-24 12:59:08 +0700

    Update style

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-M3Y637W5.mjs";import"./chunk-VRZXC3YW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-OWYUDU5V.mjs";import"./chunk-VRZXC3YW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
