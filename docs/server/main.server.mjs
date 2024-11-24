ceb906c88cae404dc15d3dea232fea1c2774e8f9
phuc.nguyendinh <ndp2193@gmail.com>
2024-11-24 12:48:49 +0700

    Update style

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XKQQA4MQ.mjs";import"./chunk-VRZXC3YW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-J7ORP4YE.mjs";import"./chunk-VRZXC3YW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
